4ab46d5bccfd2454b7c33cc0b956ec9d9d36741e
lol1kn1ght <loliknight@easygaming.su>
2022-07-27 12:48:37 +0300

    сделано разбиение содержание сайта по двум контейенерам

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { EventBus } from "Bus";
+import { api_user_data_type } from "types";
 
+const bus = new EventBus();
+bus.addEventListener("login", (data: api_user_data_type) => {
+  console.log("ЛОГИН ПРОИЗОШЕЛ");
+});
+console.log("слушатель навешан");
+
+bus.emit("login", { test: 50 });
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
