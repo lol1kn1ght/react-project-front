c0651e61810da188e6021f521cc86267bc128f9c
lol1kn1ght <loliknight@easygaming.su>
2022-07-27 12:47:31 +0300

    добавлена новая страница и стили для нее

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Home extends React.Component {
+  render() {
+    return (
+      <div className='home-block-container'>
+        <div className='home-info-block-container'>TEst</div>
+      </div>
+    );
+  }
+}
