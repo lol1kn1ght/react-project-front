3c37ef7f46034360b975153bf10eabdd0380ffa2
lol1kn1ght <loliknight@easygaming.su>
2022-07-27 20:07:49 +0300

    написан тип для профиля пользователя

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,26 @@
-export type api_user_data_type = {} | undefined;
+export type api_user_data_type = {
+  id: string;
+  username: string;
+  discriminator: string;
+  avatar?: string;
+  bot?: boolean;
+  system?: boolean;
+  mfa_enabled?: boolean;
+  banner?: boolean;
+  accent_color?: number;
+  locale?: string;
+  verified?: boolean;
+  email?: string;
+  flags: number;
+  premium_type?: number;
+  public_flags?: number;
+};
+
+export type login_data_type = {
+  token: string;
+  user: api_user_data_type;
+};
+
+export type storage_type = {
+  user?: api_user_data_type;
+};
