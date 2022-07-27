8c590bfc97c5ff5d97dff65c35fa1424dc201553
lol1kn1ght <loliknight@easygaming.su>
2022-07-27 20:03:39 +0300

    добавлен роутинг для страницы авторизации

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import { Header } from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "components/Home";
+import { Login } from "components/Login";
 
-function App() {
-  return (
-    <div className='App'>
-      <div className='navigate-menu-container'>
-        <Header />
+export class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <div className='navigate-menu-container'>
+          <Header />
+        </div>
+        <div className='content-container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login/result' element={<Login />} />
+          </Routes>
+        </div>
       </div>
-      <div className='content-container'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
