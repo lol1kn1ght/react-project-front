d4fb38c1f1c56e16750ad398cffa4bc19e0f252b
lol1kn1ght <loliknight@easygaming.su>
2022-07-29 13:11:37 +0300

    исправлена проблема с двойным рендером страницы

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
-
 import { EventsBus } from "Bus";
 import { storage_type } from "types";
-export const Bus = new EventsBus();
 
+export const Bus = new EventsBus();
 export const storage: storage_type = {
   user: {
     discriminator: "1061",
@@ -27,11 +26,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
